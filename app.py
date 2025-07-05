afac3df4db9e8021ced2d3f5c4602b0e1cbf5af6
dastaraiofficial <dastar.aiofficial@gmail.com>
2025-07-05 17:58:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def ask():
 # === Running untuk local test sahaja ===
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
